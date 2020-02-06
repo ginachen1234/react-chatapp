898d5ec38d1b56a1548b6ac0a3fc58e8a76d6eb6
gina chen <ginachen588@gmail.com>
2020-02-06 22:57:44 +0800

    finished chatapp but need to test with CometChat

---
+++ src/config.js
@@ -0,0 +1,5 @@
+export default {
+  appId: "139005468bebae2", //Enter your App ID
+  apiKey: "8999da02886f6c67d83efe0e83a8b35b8190cc9b", //Enter your API KEY
+  GUID: "testgroup", // Enter your group UID
+};
